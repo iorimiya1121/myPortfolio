ccf4073be040394519d707c88f03d11a566588bc
iori <iorimiya1121@gmail.com>
2021-09-20 11:03:46 +0900

    commit

--- app.js
+++
@@ -1,8 +0,0 @@
-
-// 360°回転
-const text = document.querySelector('.circle-textcontent p');
-text.innerHTML = text.innerText.split("").map(
-    (char,i) =>
-    `<span style="transform:rotate(${i * 7.5}deg)">${char}</span>`
-).join("")
-
--- main.js
+++ main.js
@@ -1,2 +1,143 @@
-// 文字のアニメーション
 
+
+
+particlesJS('hoge',{
+    "particles":{
+   
+  //--シェイプの設定----------
+        "number":{
+          "value": 110, //シェイプの数
+          "density":{
+            "enable":true, //シェイプの密集度を変更するか否か
+            "value_area":40 //シェイプの密集度
+          }
+        },
+        "shape":{
+          "type":"circle", //シェイプの形（circle:丸、edge:四角、triangle:三角、polygon:多角形、star:星型、image:画像）
+          "stroke":{
+            "width":0, //シェイプの外線の太さ
+            "color":"#000000" //シェイプの外線の色
+          },
+          //typeをpolygonにした時の設定
+          "polygon": {
+            "nb_sides": 5 //多角形の角の数
+          },
+          //typeをimageにした時の設定
+          "image": {
+            "src": "images/hoge.png",
+            "width": 100,
+            "height": 100
+          }
+        },
+        "color":{
+          "value":"006699" //シェイプの色
+        },
+        "opacity":{
+          "value":0.9, //シェイプの透明度
+          "random": true, //シェイプの透明度をランダムにするか否か
+          "anim":{
+            "enable":false, //シェイプの透明度をアニメーションさせるか否か
+            "speed":4, //アニメーションのスピード
+            "opacity_min":0.01, //透明度の最小値
+            "sync":false //全てのシェイプを同時にアニメーションさせるか否か
+          }
+        },
+        "size":{
+          "value":2.8, //シェイプの大きさ
+          "random":true, //シェイプの大きさをランダムにするか否か
+          "anim":{
+            "enable":true, //シェイプの大きさをアニメーションさせるか否か
+            "speed":1.5, //アニメーションのスピード
+            "size_min":0.0001, //大きさの最小値
+            "sync":true //全てのシェイプを同時にアニメーションさせるか否か
+          }
+        },
+  //--------------------
+  
+  //--線の設定----------
+        "line_linked":{
+          "enable": false, //線を表示するか否か
+          "distance": 80, //線をつなぐシェイプの間隔
+          "color":"#222222", //線の色
+          "opacity":0.1, //線の透明度
+          "width":1 //線の太さ
+        },
+  //--------------------
+  
+  //--動きの設定----------
+        "move":{
+          "speed":3.5, //シェイプの動くスピード
+          "straight":false, //個々のシェイプの動きを止めるか否か
+          "direction":"top", //エリア全体の動き(none、top、top-right、right、bottom-right、bottom、bottom-left、left、top-leftより選択)
+          "out_mode":"out" //エリア外に出たシェイプの動き(out、bounceより選択)
+        }
+  //--------------------
+  
+      },
+   
+      "interactivity":{
+        "detect_on":"canvas",
+        "events":{
+  
+  //--マウスオーバー時の処理----------
+          "onhover":{
+            "enable":false, //マウスオーバーが有効か否か
+            "mode":"grub" //マウスオーバー時に発動する動き(下記modes内のgrab、repulse、bubbleより選択)
+          },
+  //--------------------
+  
+  //--クリック時の処理----------
+          "onclick":{
+            "enable":true, //クリックが有効か否か
+            "mode":"push" //クリック時に発動する動き(下記modes内のgrab、repulse、bubble、push、removeより選択)
+          },
+  //--------------------
+  
+        },
+   
+        "modes":{
+  
+  //--カーソルとシェイプの間に線が表示される----------
+          "grab":{
+            "distance":400, //カーソルからの反応距離
+            "line_linked":{
+              "opacity":1 //線の透明度
+            }
+          },
+  //--------------------
+  
+  //--シェイプがカーソルから逃げる----------
+          "repulse":{
+            "distance":200 //カーソルからの反応距離
+          },
+  //--------------------
+  
+  //--シェイプが膨らむ----------
+          "bubble":{
+            "distance":400, //カーソルからの反応距離
+            "size":40, //シェイプの膨らむ大きさ
+            "opacity":8, //膨らむシェイプの透明度
+            "duration":2, //膨らむシェイプの持続時間(onclick時のみ)
+            "speed":3 //膨らむシェイプの速度(onclick時のみ)
+          },
+  //--------------------
+  
+  //--シェイプが増える----------
+          "push":{
+            "particles_nb":4 //増えるシェイプの数
+          },
+  //--------------------
+  
+  //--シェイプが減る----------
+          "remove":{
+            "particles_nb":2 //減るシェイプの数
+          }
+  //--------------------
+  
+        }
+      },
+      "retina_detect":true, //Retina Displayを対応するか否か
+      "resize":true //canvasのサイズ変更にわせて拡大縮小するか否か
+    }
+  );
+  
